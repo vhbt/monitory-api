e42cc5b255cb5b9e93f06c1e9ca77a296e2b9449
Vitor Hariel <vhbarauna@gmail.com>
2019-08-18 19:19:12 -0300

    add: fix database config env vars

--- src/config/database.js
+++ src/config/database.js
@@ -4,8 +4,8 @@ module.exports = {
   dialect: process.env.DB_DIALECT,
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
-  password: process.env_DB_PASS,
-  database: process.env_DB_NAME,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
