4235bc6e2723413c5c620156dcf76c084480c7da
Vitor Hariel <vhbarauna@gmail.com>
2019-08-23 22:13:12 -0300

    add: cors

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import Youch from 'youch';
+import cors from 'cors';
 import { resolve } from 'path';
 
 import routes from './routes';
@@ -17,6 +18,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use('/files', express.static(resolve(__dirname, 'files')));
   }
