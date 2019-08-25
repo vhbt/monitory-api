80eabe4506363ff5b7a7103430b2f74370a7b627
Vitor Hariel <vhbarauna@gmail.com>
2019-08-25 18:58:24 -0300

    add: sentry

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 import 'dotenv/config';
 
+import * as Sentry from '@sentry/node';
 import express from 'express';
 import Youch from 'youch';
 import cors from 'cors';
 import { resolve } from 'path';
 
 import routes from './routes';
+import SentryConfig from './config/sentry';
+
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(SentryConfig);
 
     this.middlewares();
     this.routes();
@@ -18,6 +23,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
     this.server.use('/files', express.static(resolve(__dirname, 'files')));
@@ -25,6 +31,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 
   exceptionHandler() {
